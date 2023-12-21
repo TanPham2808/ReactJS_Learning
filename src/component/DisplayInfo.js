98c430a4a28baa525c515b28b3cb4670340923d3
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2023-12-21 20:33:58 +0700

    Video #25 Áp dụng câu điều kiện vào React

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -2,20 +2,40 @@ import React from "react";
 
 class DisplayInfo extends React.Component {
 
+    state={
+        isShowListUser: true
+    }
+    
+    handleShowHide = () => {
+        this.setState({
+            isShowListUser : !this.state.isShowListUser
+        })
+    }
+
     render() {
         // Sử dụng Destructuring 
         const { listUser } = this.props; // object
         return (
             <div>
-                {listUser.map((user) => {
-                    return (
-                        <div key={user.id}>
-                            <div>My name's {user.name}</div>
-                            <div>My age's {user.age}</div>
-                            <hr />
-                        </div>
-                    )
-                })}
+                <div>
+                    <span onClick={()=>{this.handleShowHide()}}>{this.state.isShowListUser ? "Hide list user" : "Show list user"} </span>
+                </div>
+
+                {this.state.isShowListUser &&
+                    <div>
+                        {listUser.map((user) => {
+                            return (
+                                <div>
+                                    <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                                        <div>My name's {user.name}</div>
+                                        <div>My age's {user.age}</div>
+                                        <hr />
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
             </div>
         )
     }
