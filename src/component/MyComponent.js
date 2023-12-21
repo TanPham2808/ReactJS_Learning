8ebde2cd7b45e7fe2241f827eb03f0bea9111540
Tan Pham <tan.phamx@dmchosting.ca>
2023-12-21 14:23:12 +0700

    Video 19: Xử lý Event mouse

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -4,13 +4,28 @@ class MyComponent extends React.Component {
 
     state = {
         name: 'XTan',
-        add: 'HCM',
+        address: 'HCM',
         age: 32
+    }
+
+    // handle sự kiện click chuột
+    handleClick() {
+        console.log('>>Click me button');
+    }
+
+    // handle sự kiện hover chuột
+    handleHover(event) {
+        console.log('>>> Hover click and toa do X:', event.pageX);
     }
 
     render() {
         return (
-            <div>My name is {this.state.name} </div>
+            <div>
+                My name is {this.state.name} and I'm from {this.state.address}
+                <button onClick={this.handleClick}>Click me</button>
+                <button onMouseOver={this.handleHover}>Hover me</button>
+            </div>
+
         )
     }
 }
