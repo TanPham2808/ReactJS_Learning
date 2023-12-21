25838e01ddcd83d86df9b6e31757af98dc4238c3
Tan Pham <tan.phamx@dmchosting.ca>
2023-12-21 16:22:19 +0700

    Video #22: Tách code phân ra cha và con

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -1,39 +1,12 @@
 import React from "react";
+import UserInfo from "./UserInfo";
 
 class MyComponent extends React.Component {
-
-    state = {
-        name: 'XTan',
-        address: 'HCM',
-        age: 32
-    }
-
-    // Cập nhật lại state khi onchange trong input
-    handleOnChange = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        // Ngăn chặn page reload lại
-        event.preventDefault();
-        console.log(this.state);
-    }
-
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm {this.state.age} year old
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input
-                        type="text"
-                        onChange={(event) => { this.handleOnChange(event) }}
-                    ></input>
-                    <button>submit</button>
-                </form>
+                <UserInfo />
             </div>
-
         )
     }
 }
