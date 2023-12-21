47012ef12f0b2e140d651dc7873e207c7dd18cc9
Tan Pham <tan.phamx@dmchosting.ca>
2023-12-21 14:57:51 +0700

    Video #20: Cập nhật lại state trong React

--- src/component/MyComponent.js
+++ src/component/MyComponent.js
@@ -9,8 +9,14 @@ class MyComponent extends React.Component {
     }
 
     // handle sự kiện click chuột
-    handleClick() {
-        console.log('>>Click me button');
+    handleClick = (event) => {
+
+        // Cập nhật lại state (dùng keyword 'this') => Khi cập nhật lại state thì func render sẽ được call lại làm thay đổi UI
+        this.setState({
+            name: 'Ku Bim',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+        // 'this' ở đây để ám chỉ chúng ta đang thao tác với component nào và muốn làm gì bên trong component
     }
 
     // handle sự kiện hover chuột
@@ -21,9 +27,11 @@ class MyComponent extends React.Component {
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm from {this.state.address}
-                <button onClick={this.handleClick}>Click me</button>
+                My name is {this.state.name} and I'm {this.state.age} year old
+
                 <button onMouseOver={this.handleHover}>Hover me</button>
+
+                <button onClick={(event) => { this.handleClick(event) }}>Click me</button>
             </div>
 
         )
