b6b4f193f13add365b0b1174352980f019f553f4
Tan Pham <tan.phamx@dmchosting.ca>
2023-12-22 15:32:52 +0700

    Video #34: Chuyển đổi Class Component thành 1 function Component

--- src/component/DisplayInfo.js
+++ src/component/DisplayInfo.js
@@ -1,57 +1,63 @@
 import React from "react";
 import './DisplayInfo.scss';
 
-class DisplayInfo extends React.Component {
+// class DisplayInfo extends React.Component {
 
-    // Cách viết chuẩn đối với Class
-    constructor(props) {
+//     render() {
+//         // Sử dụng Destructuring 
+//         const { listUser } = this.props; // object
+//         return (
+//             // Nên có 1 class cha bọc ngoài để viết scss cho dễ
+//             <div className="display-info-container">
+//                 {true &&
+//                     <>
+//                         {listUser.map((user) => {
+//                             return (
+//                                 <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+//                                     <div>
+//                                         <div>My name's {user.name}</div>
+//                                         <div>My age's {user.age}</div>
+//                                     </div>
+//                                     <div>
+//                                         <button onClick={() => { this.props.handleDeleteUser(user.id) }}>X</button>
+//                                     </div>
+//                                     <hr />
+//                                 </div>
+//                             )
+//                         })}
+//                     </>
+//                 }
+//             </div>
+//         )
+//     }
+// }
 
-        // Component con sẽ nhận đầy đủ props của thằng cha truyền xuống
-        super(props);
+const DisplayInfo = (props) => {
+    const { listUser } = props; // object
 
-        // Khởi tạo state cho 1 component
-        this.state = {
-            isShowListUser: true
-        }
-    }
-
-    handleShowHide = () => {
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
-
-    render() {
-        // Sử dụng Destructuring 
-        const { listUser } = this.props; // object
-        return (
-            // Nên có 1 class cha bọc ngoài để viết scss cho dễ
-            <div className="display-info-container">
-                <div>
-                    <span onClick={() => { this.handleShowHide() }}>{this.state.isShowListUser ? "Hide list user" : "Show list user"} </span>
-                </div>
-
-                {this.state.isShowListUser &&
-                    <>
-                        {listUser.map((user) => {
-                            return (
-                                <div key={user.id} className={user.age > 18 ? "green" : "red"}>
-                                    <div>
-                                        <div>My name's {user.name}</div>
-                                        <div>My age's {user.age}</div>
-                                    </div>
-                                    <div>
-                                        <button onClick={() => { this.props.handleDeleteUser(user.id) }}>X</button>
-                                    </div>
-                                    <hr />
+    return (
+        <div className="display-info-container">
+            {true &&
+                <>
+                    {listUser.map((user) => {
+                        return (
+                            <div key={user.id} className={user.age > 18 ? "green" : "red"}>
+                                <div>
+                                    <div>My name's {user.name}</div>
+                                    <div>My age's {user.age}</div>
                                 </div>
-                            )
-                        })}
-                    </>
-                }
-            </div>
-        )
-    }
+                                <div>
+                                    <button onClick={() => { props.handleDeleteUser(user.id) }}>X</button>
+                                </div>
+                                <hr />
+                            </div>
+                        )
+                    })}
+                </>
+            }
+        </div>
+    )
+
 }
 
 export default DisplayInfo
