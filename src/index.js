f8d509b88400ededa20e7769e6451a41d015eedf
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2023-12-24 09:38:13 +0700

    Vide0 #52: Design homepage_1

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-  BrowserRouter, Routes, Route, MemoryRouter
+  BrowserRouter, Routes, Route
 } from "react-router-dom";
 import User from './component/User/User';
 import Admin from './component/Admin/Admin';
