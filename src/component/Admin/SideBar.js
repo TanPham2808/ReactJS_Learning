a3eb4984ae8453ff84da413738340d5989a3a84e
Tan Pham <tan.phamx@dmchosting.ca>
2023-12-25 17:31:10 +0700

    Video #56: Xây dựng Route cho Admin - build Modal thêm mới User

--- src/component/Admin/SideBar.js
+++ src/component/Admin/SideBar.js
@@ -1,12 +1,16 @@
 import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import { Link } from 'react-router-dom';
 
 const SideBar = ({ collapsed }) => {
     return (
         <>
             <Sidebar collapsed={collapsed}>
                 <Menu>
+                    <MenuItem component={<Link to="/admins" />}> Dashboard </MenuItem>
                     <SubMenu label="Chức năng">
-                        <MenuItem> Quản lý Users </MenuItem>
+                        <MenuItem component={<Link to="/admins/manage-users" />}>
+                            Quản lý Users
+                        </MenuItem>
                         <MenuItem> Quản lý Bài Quiz </MenuItem>
                         <MenuItem> Quản lý Câu Hỏi </MenuItem>
                     </SubMenu>
--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import {
 import User from './component/User/User';
 import Admin from './component/Admin/Admin';
 import HomePage from './component/Home/HomePage';
+import ManageUser from './component/Admin/Content/ManageUser';
+import DashBoard from './component/Admin/Content/DashBoard';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -23,7 +25,10 @@ root.render(
           <Route index element={<HomePage />} />
           <Route path="users" element={<User />} />
         </Route>
-        <Route path="admins" element={<Admin />} />
+        <Route path="/admins" element={<Admin />} >
+          <Route index element={<DashBoard />} />
+          <Route path="manage-users" element={<ManageUser />} />
+        </Route>
       </Routes>
     </BrowserRouter>
     {/* </React.StrictMode> */}
