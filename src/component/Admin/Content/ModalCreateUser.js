bfe90411abf9c050aeb1fba6ded10061bf6ccf40
Tan Pham <tan.phamx@dmchosting.ca>
2023-12-26 10:18:19 +0700

    Video #59: Dùng Toastify để thông báo đến người dùng

--- src/component/Admin/Content/ModalCreateUser.js
+++ src/component/Admin/Content/ModalCreateUser.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { CgAdd } from "react-icons/cg";
-import axios from 'axios'
+import axios from 'axios';
+import { toast } from 'react-toastify';
 
 const ModalCreateUser = (props) => {
     const { show, setShow } = props;
@@ -31,10 +32,28 @@ const ModalCreateUser = (props) => {
         }
     }
 
+    const validateEmail = (email) => {
+        return String(email)
+            .toLowerCase()
+            .match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            );
+    };
+
     const handleSubmitCreateUser = async () => {
         // validate
+        const isValidEmail = validateEmail(email);
+        if (!isValidEmail) {
+            toast.error('Invalid Email');
+            return;
+        }
 
-        // call API
+        if (!password) {
+            toast.error('Invalid Password');
+            return;
+        }
+
+        // call API                 
         const data = new FormData();
         data.append('email', email);
         data.append('password', password);
@@ -42,8 +61,15 @@ const ModalCreateUser = (props) => {
         data.append('role', role);
         data.append('userImage', image);
 
-        let res = await axios.post('http://localhost:8081/api/v1/participant', data)
-        console.log(res);
+        let res = await axios.post('http://172.31.23.175:8081/api/v1/participant', data)
+        if (res.data && res.data.EC === 0) {
+            toast.success(res.data.EM);
+            handleClose();
+        }
+
+        if (res.data && res.data.EC !== 0) {
+            toast.error(res.data.EM);
+        }
     }
 
     return (
