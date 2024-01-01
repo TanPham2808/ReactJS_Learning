5e8d9fba81a1865daa52fc7b405c82d4564cf03a
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-01-01 14:25:04 +0700

    Video #83: Xử lý data trên fontEnd (sử dụng lodash)

--- src/component/User/DetailQuiz.js
+++ src/component/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom'; // Hook của thư viện
 import { getDataQuiz } from '../../services/ApiServices'
+import _, { values } from 'lodash';
 
 const DetailQuiz = (props) => {
     // Lấy param trên URL
@@ -14,7 +15,26 @@ const DetailQuiz = (props) => {
 
     const fetchQuestion = async () => {
         const data = await getDataQuiz(quizId);
-        console.log("Check data >>>>: ", data);
+        if (data && data.EC === 0) {
+            let dataFilter = _.chain(data.DT)
+                .groupBy("id")
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    })
+
+                    return { questionId: key, answers, questionDescription, image }
+                })
+                .value();
+            console.log("datafgilter >>> : ", dataFilter);
+        }
+
     }
 
 
