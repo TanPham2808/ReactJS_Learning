d80b8307dd79925a08e415f1849b7f107768c8ad
TANPHAM\xuant <xuantan.pham2808@gmail.com>
2024-01-01 15:53:47 +0700

    Video #84: - Design UI DetailQuiz - Truyền mutile paramater trong navigate - Sử dụng uselocation từ hook react-router-dom

--- src/component/User/DetailQuiz.js
+++ src/component/User/DetailQuiz.js
@@ -1,12 +1,14 @@
 import { useEffect } from 'react';
-import { useParams } from 'react-router-dom'; // Hook của thư viện
+import { useParams, useLocation } from 'react-router-dom'; // Hook của thư viện
 import { getDataQuiz } from '../../services/ApiServices'
 import _, { values } from 'lodash';
+import './DetailQuiz.scss';
 
 const DetailQuiz = (props) => {
     // Lấy param trên URL
     const params = useParams();
-
+    // lấy data được truyền từ ListQuiz
+    const location = useLocation();
     const quizId = params.id;
 
     useEffect(() => {
@@ -16,6 +18,7 @@ const DetailQuiz = (props) => {
     const fetchQuestion = async () => {
         const data = await getDataQuiz(quizId);
         if (data && data.EC === 0) {
+            // Sử dụng kỹ thuật groupby data bằng lodash
             let dataFilter = _.chain(data.DT)
                 .groupBy("id")
                 .map((value, key) => {
@@ -32,15 +35,37 @@ const DetailQuiz = (props) => {
                     return { questionId: key, answers, questionDescription, image }
                 })
                 .value();
-            console.log("datafgilter >>> : ", dataFilter);
         }
 
     }
 
 
     return (
-        <div className="detail-quiz">
-            Detail Quiz
+        <div className="detail-quiz-container">
+            <div className='left-content'>
+                <div className='title'>
+                    Quiz: {quizId} - {location?.state.quizTitle}
+                </div>
+                <hr />
+                <div className='q-body'>
+                    <img />
+                </div>
+                <div className='q-content'>
+                    <div className='question'>Question 1 : How are you ? </div>
+                    <div className='answer'>
+                        <div className='a-child'>A. asdwd</div>
+                        <div className='a-child'>B. asdwd</div>
+                        <div className='a-child'>C. asdwd</div>
+                    </div>
+                </div>
+                <div className='footer'>
+                    <button className='btn btn-secondary'>Prev</button>
+                    <button className='btn btn-primary'>Next</button>
+                </div>
+            </div>
+            <div className='right-content'>
+                count down
+            </div>
         </div>
     )
 }
